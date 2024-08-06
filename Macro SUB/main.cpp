ce707fa1d0b3d2699cbe171c4c54012f71e88d58
Alexander Nesterov <alex18nest@icloud.com>
2024-08-06 16:34:55 -0300

    Initial Commit

---
+++ Macro SUB/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Macro SUB
+//
+//  Created by Александр Нестеров on 8/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
